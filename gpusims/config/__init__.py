1aabea380c86b5afb762c70c2d9f9be1665c5f32
romnnn <roman.dahm@gmx.de>
2022-12-13 19:15:26 +0100

    work on figures

--- gpusims/config/__init__.py
+++ gpusims/config/__init__.py
@@ -3,17 +3,18 @@ import gpusims.config.gpgpusim  # noqa: F401
 import gpusims.config.tejas  # noqa: F401
 import gpusims.config.multi2sim  # noqa: F401
 import gpusims.config.macsim  # noqa: F401
+from collections import namedtuple
 
-
-class Config:
-    def __init__(self, name, path):
-        if path is not None:
-            assert path.is_dir()
-        self.name = name
-        self.path = path
-
-    def __repr__(self):
-        return "{}({})".format(self.__class__.__name__, self.path)
+Config = namedtuple("Config", ["name", "path", "spec"])
+#class Config:
+#    def __init__(self, name, path):
+#        if path is not None:
+#            assert path.is_dir()
+#        self.name = name
+#        self.path = path
+#
+#    def __repr__(self):
+#        return "{}({})".format(self.__class__.__name__, self.path)
 
 
 def parse_configs(path):
@@ -29,5 +30,6 @@ def parse_configs(path):
             configs[name.lower()] = Config(
                 name=name,
                 path=path.parent / config["path"],
+                spec=config["spec"],
             )
     return configs
