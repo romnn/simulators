aff6073457c0cb949e8c6f5e126bd659a640157a
romnnn <roman.dahm@gmx.de>
2022-12-14 04:53:49 +0100

    improve tejas parser

--- tools/tejas-parse/main.rs
+++ tools/tejas-parse/main.rs
@@ -28,6 +28,16 @@ struct Stats {
     sim_time: Option<Duration>,
     total_inst_count: Option<u64>,
     total_cycle_count: Option<u64>,
+    dram_total_reads: u64,
+    dram_total_writes: u64,
+    dram_avg_read_latency: Option<f64>,
+    total_instr_cache_access: Option<u64>,
+    total_const_cache_access: Option<u64>,
+    total_shared_cache_access: Option<u64>,
+    total_instr_cache_misses: Option<u64>,
+    total_const_cache_misses: Option<u64>,
+    total_shared_cache_misses: Option<u64>,
+
     kips: Option<f64>,
     total_ipc: Option<f64>,
 }
@@ -52,6 +62,18 @@ where
     match *opt {
         Some(ref dt) => serializer.serialize_some(&dt.timestamp()),
         None => serializer.serialize_none(),
+    }
+}
+
+fn mean<'a, T>(values: &'a Vec<T>) -> Option<f64>
+where
+    T: std::iter::Sum<&'a T> + 'a,
+    f64: From<T>,
+{
+    if values.is_empty() {
+        None
+    } else {
+        Some(f64::from(values.iter().sum::<T>()) / values.len() as f64)
     }
 }
 
@@ -133,6 +155,28 @@ fn main() -> Result<()> {
         pub static ref TOTAL_IPC_REGEX: Regex = multiline_regex!(
             r"\[Simulator Time\][\s\S]*TOTAL INSTRUCTIONS PER CYCLE\s*=\s*(\d+\.?\d*)\s*$"
         );
+        pub static ref TOTAL_INSTR_CACHE_ACCESS: Regex = multiline_regex!(
+            r"Cache details\n[\s\S]*Total Instruction Cache Access\s*(\d+\.?\d*)\s*$"
+        );
+        pub static ref TOTAL_INSTR_CACHE_MISSES: Regex = multiline_regex!(
+            r"Cache details\n[\s\S]*Total Instruction Cache Misses\s*(\d+\.?\d*)\s*$"
+        );
+        pub static ref TOTAL_CONST_CACHE_ACCESS: Regex = multiline_regex!(
+            r"Cache details\n[\s\S]*Total Constant Cache Access\s*(\d+\.?\d*)\s*$"
+        );
+        pub static ref TOTAL_CONST_CACHE_MISSES: Regex = multiline_regex!(
+            r"Cache details\n[\s\S]*Total Constant Cache Misses\s*(\d+\.?\d*)\s*$"
+        );
+        pub static ref TOTAL_SHARED_CACHE_ACCESS: Regex =
+            multiline_regex!(r"Cache details\n[\s\S]*Total Shared Cache Access\s*(\d+\.?\d*)\s*$");
+        pub static ref TOTAL_SHARED_CACHE_MISSES: Regex =
+            multiline_regex!(r"Cache details\n[\s\S]*Total Shared Cache Misses\s*(\d+\.?\d*)\s*$");
+        pub static ref RAM_AVG_READ_LATENCY: Regex =
+            multiline_regex!(r"For channel \d+:\n[\s\S]*Average Read Latency:\s*(\d+\.?\d*)");
+        pub static ref RAM_TOTAL_READS: Regex =
+            multiline_regex!(r"Bank\s*\d+\s*::\s*Reads\s*:\s*(\d+)\s*\|\s*Writes\s*:\s*\d+");
+        pub static ref RAM_TOTAL_WRITES: Regex =
+            multiline_regex!(r"Bank\s*\d+\s*::\s*Reads\s*:\s*\d+\s*\|\s*Writes\s*:\s*(\d+)");
     }
 
     let kips = extract("KIPS", &KIPS_REGEX, &raw_stats);
@@ -147,6 +191,47 @@ fn main() -> Result<()> {
         sim_time,
         kips,
         total_inst_count,
+        dram_total_reads: extract_all("dram_total_reads", &RAM_TOTAL_READS, &raw_stats)
+            .iter()
+            .sum(),
+        dram_total_writes: extract_all("dram_total_writes", &RAM_TOTAL_WRITES, &raw_stats)
+            .iter()
+            .sum(),
+        dram_avg_read_latency: mean::<f64>(&extract_all(
+            "dram_avg_read_latency",
+            &RAM_AVG_READ_LATENCY,
+            &raw_stats,
+        )),
+        total_instr_cache_access: extract(
+            "total_instr_cache_access",
+            &TOTAL_INSTR_CACHE_ACCESS,
+            &raw_stats,
+        ),
+        total_const_cache_access: extract(
+            "total_const_cache_access",
+            &TOTAL_CONST_CACHE_ACCESS,
+            &raw_stats,
+        ),
+        total_shared_cache_access: extract(
+            "total_shared_cache_access",
+            &TOTAL_SHARED_CACHE_ACCESS,
+            &raw_stats,
+        ),
+        total_instr_cache_misses: extract(
+            "total_instr_cache_misses",
+            &TOTAL_INSTR_CACHE_MISSES,
+            &raw_stats,
+        ),
+        total_const_cache_misses: extract(
+            "total_const_cache_misses",
+            &TOTAL_CONST_CACHE_MISSES,
+            &raw_stats,
+        ),
+        total_shared_cache_misses: extract(
+            "total_shared_cache_misses",
+            &TOTAL_SHARED_CACHE_MISSES,
+            &raw_stats,
+        ),
         total_cycle_count: extract("total_cycle_count", &TOTAL_CYCLE_REGEX, &raw_stats),
         total_ipc: extract("total_ipc", &TOTAL_IPC_REGEX, &raw_stats),
         emulator_type: extract("emulator_type", &EMULATOR_TYPE_REGEX, &raw_stats),
@@ -157,16 +242,6 @@ fn main() -> Result<()> {
     };
     println!("stats: {:#?}", &stats);
 
-    // let csv_stats: HashMap<String, String> = [
-    //     ("sim_time", String::serialize(stats.sim_time)),
-    //      // .serialize(.to_string()),
-    //     // ("sim_time", stats.sim_time.to_string()),
-    //     // ("KIPS", stats.kips.map(.to_string()),
-    // ]
-    // .iter()
-    // .collect();
-
-    // csv_writer.write_record(csv_stats.keys())?;
     csv_writer.serialize(&stats)?;
 
     let duration = start.elapsed();
@@ -174,17 +249,11 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn extract<T>(name: impl AsRef<str>, reg: &Regex, s: impl AsRef<str>) -> Option<T>
+fn filter_valid<T, E>(name: impl AsRef<str>, value: Option<Result<T, E>>) -> Option<T>
 where
-    T: std::str::FromStr + std::fmt::Display,
-    <T as std::str::FromStr>::Err: std::fmt::Debug,
+    E: std::fmt::Debug,
 {
-    match reg
-        .captures(s.as_ref())
-        .and_then(|c| c.get(1))
-        .map(|m| m.as_str().trim())
-        .map(|m| m.parse::<T>())
-    {
+    match value {
         Some(Err(err)) => {
             eprintln!("failed to parse {}: {:?}", name.as_ref(), &err);
             None
@@ -198,4 +267,30 @@ where
             None
         }
     }
+}
+
+fn extract<T>(name: impl AsRef<str>, reg: &Regex, s: impl AsRef<str>) -> Option<T>
+where
+    T: std::str::FromStr + std::fmt::Display,
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    filter_valid(
+        name,
+        reg.captures(s.as_ref())
+            .and_then(|c| c.get(1))
+            .map(|m| m.as_str().trim().parse::<T>()),
+    )
+}
+
+fn extract_all<T>(name: impl AsRef<str>, reg: &Regex, s: impl AsRef<str>) -> Vec<T>
+where
+    T: std::str::FromStr + std::fmt::Display,
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    let values: Vec<T> = reg
+        .captures_iter(s.as_ref())
+        .map(|c| c.get(1).map(|m| m.as_str().trim().parse::<T>()))
+        .filter_map(|c| filter_valid(name.as_ref(), c))
+        .collect();
+    values
 }
